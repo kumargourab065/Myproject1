c621843d58bb606b8e0298184ed3df64d9e57680
kumargourab065 <122559931+kumargourab065@users.noreply.github.com>
2023-01-29 23:40:15 +0530

    Add files via upload

---
+++ Oprator.java
@@ -0,0 +1,9 @@
+public class Oprator {
+    public static void main(String args[]){
+        int x=7;
+        System.out.println(--x);
+        System.out.println(x--);
+        System.out.println(++x);
+        System.out.println(x++);
+        System.out.println(x);
+    }}
